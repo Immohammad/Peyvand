7fe0156e6bb93409e2fd3f2b4164477debb62e78
Mohammad <mo.movahedinia@gmail.com>
2022-08-11 16:33:32 +0430

    Adding Analysis and Design

--- src/components/signLogin.jsx
+++ src/components/signLogin.jsx
@@ -2,37 +2,38 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignLogin = (props) => {
-  const [name, setName] = useState("");
-  const [code, setCode] = useState("");
+  const [userName, setUserName] = useState("");
   const [pass, setPass] = useState("");
+  const [passRepeat, setPassRepeat] = useState("");
+  const [position, setPositiont] = useState("");
   const [isLoggedin, setIsLoggedin] = useState(false);
   const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
-    navigate("/news");
-    event.preventDefault();
-  };
-
-  const handleCode = (Event) => {
-    setCode(Event.target.value);
-  };
-
-  const handlePass = (Event) => {
-    setPass(Event.target.value);
-  };
-
-  const handleName = (Event) => {
-    setName(Event.target.value);
-  };
 
   const signup = () => {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("signInForm").style.display = "block";
   };
-
-  const SignLogin = () => {
+  const login = () => {
     document.getElementById("signInForm").style.display = "none";
     document.getElementById("loginForm").style.display = "block";
+  };
+  
+  const handleUserName = (Event) => {
+    setUserName(Event.target.value);
+  };
+  const handlePass = (Event) => {
+    setPass(Event.target.value);
+  };
+  const handlePassRepeat = (Event) => {
+    setPassRepeat(Event.target.value);
+  };
+  const handlePosition = (Event) => {
+    setPositiont(Event.target.value);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/news");
   };
 
   return (
@@ -41,30 +42,19 @@ const SignLogin = (props) => {
         <button className="signs" onClick={signup}>
           ثبت نام
         </button>
-        <button className="signs" onClick={SignLogin}>
+        <button className="signs" onClick={login}>
           ورود
         </button>
       </div>
 
       <form onSubmit={handleSubmit} id="signInForm">
         <label>
-          نام و نام خانوادگی
+          نام کاربری
           <br />
           <input
             type="text"
-            value={name}
-            onChange={handleName}
-            size="40"
-            required
-          ></input>
-        </label>
-        <label>
-          شماره پرسنلی
-          <br />
-          <input
-            type="text"
-            value={code}
-            onChange={handleCode}
+            value={userName}
+            onChange={handleUserName}
             size="40"
             required
           ></input>
@@ -82,17 +72,38 @@ const SignLogin = (props) => {
             required
           ></input>
         </label>
+        <label>
+          تکرار رمز عبور
+          <br />
+          <input
+            type="password"
+            value={passRepeat}
+            onChange={handlePassRepeat}
+            size="40"
+            placeholder="گذرواژۀ واردشده در مرحلۀ قبل را دوباره وارد کنید"
+            required
+          ></input>
+        </label>
+        <label>
+          نقش
+          <br />
+          <select value={position} onChange={handlePosition} required style={{display: "block"}}>
+            <option value="0">دانشجو</option>
+            <option value="1">استاد</option>
+            <option value="2">شرکت</option>
+          </select>
+        </label>
         <input type="submit" id="submit" value="ثبت"></input>
       </form>
 
       <form id="loginForm" style={{ display: "none" }}>
-        <label>
-          شماره پرسنلی
+      <label>
+          نام کاربری
           <br />
           <input
             type="text"
-            value={code}
-            onChange={handleCode}
+            value={userName}
+            onChange={handleUserName}
             size="40"
             required
           ></input>
