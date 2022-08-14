551d616312d9e60f87ff0f29bcbf016fc2268bd8
Mohammad <mo.movahedinia@gmail.com>
2022-08-14 23:23:07 +0430

    signup ui completed

--- src/components/signLogin.jsx
+++ src/components/signLogin.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
+// import './styling.css';
 import { useNavigate } from "react-router-dom";
 
-const SignLogin = (props) => {
+const SignLogin = () => {
+  // states that have signup data
   const [userName, setUserName] = useState("");
   const [pass, setPass] = useState("");
   const [passRepeat, setPassRepeat] = useState("");
-  const [position, setPositiont] = useState("");
+  const [position, setPositiont] = useState("0");
+
+  //expanded forms data
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [gender, setGender] = useState("");
+  const [studentId, setStudentId] = useState("");
+
+  const [teacherId, setTeacherId] = useState("");
+
+  const [companyName, setCompanyName] = useState("");
+  const [companyCode, setCompanyCode] = useState("");
+
+  //other states
   const [isLoggedin, setIsLoggedin] = useState(false);
   const navigate = useNavigate();
 
+  //handlers
   const signup = () => {
     document.getElementById("loginForm").style.display = "none";
-    document.getElementById("signInForm").style.display = "block";
+    document.getElementById("signupForm").style.display = "block";
   };
   const login = () => {
-    document.getElementById("signInForm").style.display = "none";
+    document.getElementById("signupForm").style.display = "none";
     document.getElementById("loginForm").style.display = "block";
   };
-  
+
   const handleUserName = (Event) => {
     setUserName(Event.target.value);
   };
@@ -30,15 +46,68 @@ const SignLogin = (props) => {
   const handlePosition = (Event) => {
     setPositiont(Event.target.value);
   };
-  
-  const handleSubmit = (event) => {
+
+  const handleName = (Event) => {
+    setName(Event.target.value);
+  };
+  const handleCode = (Event) => {
+    setCode(Event.target.value);
+  };
+  const handleGender = (Event) => {
+    setGender(Event.target.value);
+  };
+  const handleStudentId = (Event) => {
+    setStudentId(Event.target.value);
+  };
+  const handleTeacherId = (Event) => {
+    setTeacherId(Event.target.value);
+  };
+  const handleCompanyName = (Event) => {
+    setCompanyName(Event.target.value);
+  };
+  const handleCompanyCode = (Event) => {
+    setCompanyCode(Event.target.value);
+  };
+
+  //submits
+  const handleButtonToNext = (event) => {
     event.preventDefault();
-    navigate("/news");
+    document.getElementById("signupForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("buttonsContainer").style.display = "none";
+    if (position === "0") {
+      document.getElementById("signupContinueStudent").style.display = "block";
+    } else if (position === "1") {
+      document.getElementById("signupContinueTeacher").style.display = "block";
+    } else if (position === "2") {
+      document.getElementById("signupContinueCompany").style.display = "block";
+    }
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    ///////////////////////////////
+    navigate("/");
+  };
+  const handleStudentSignup = (event) => {
+    event.preventDefault();
+    ///////////////////////////////
+    navigate("/");
+  };
+  const handleTeacherSignup = (event) => {
+    event.preventDefault();
+    ///////////////////////////////
+    navigate("/");
+  };
+  const handleCompanySignup = (event) => {
+    event.preventDefault();
+    ///////////////////////////////
+    navigate("/");
   };
 
   return (
     <div id="containForm">
-      <div id="signsContainer">
+      <div id="buttonsContainer">
         <button className="signs" onClick={signup}>
           ثبت نام
         </button>
@@ -47,7 +116,12 @@ const SignLogin = (props) => {
         </button>
       </div>
 
-      <form onSubmit={handleSubmit} id="signInForm">
+      {/* ////////////////////////////////////////////////////asli */}
+      <form
+        onSubmit={handleButtonToNext}
+        id="signupForm"
+        style={{ display: "block" }}
+      >
         <label>
           نام کاربری
           <br />
@@ -56,6 +130,7 @@ const SignLogin = (props) => {
             value={userName}
             onChange={handleUserName}
             size="40"
+            placeholder="نام کاربری یکتایی وارد کنید"
             required
           ></input>
         </label>
@@ -72,6 +147,7 @@ const SignLogin = (props) => {
             required
           ></input>
         </label>
+
         <label>
           تکرار رمز عبور
           <br />
@@ -84,20 +160,28 @@ const SignLogin = (props) => {
             required
           ></input>
         </label>
-        <label>
+
+        <label style={{ display: "block" }}>
           نقش
           <br />
-          <select value={position} onChange={handlePosition} required style={{display: "block"}}>
+          <select
+            value={position}
+            onChange={handlePosition}
+            required
+            style={{ display: "block" }}
+          >
             <option value="0">دانشجو</option>
             <option value="1">استاد</option>
             <option value="2">شرکت</option>
           </select>
         </label>
-        <input type="submit" id="submit" value="ثبت"></input>
+        <input type="submit" className="loginButtons" value="مرحلۀ بعد"></input>
       </form>
+      {/* //////////////////////////////////////////////////// asli finish*/}
 
-      <form id="loginForm" style={{ display: "none" }}>
-      <label>
+      {/* //////////////////////////////////////////////////// login*/}
+      <form id="loginForm" onSubmit={handleLogin} style={{ display: "none" }}>
+        <label>
           نام کاربری
           <br />
           <input
@@ -119,8 +203,159 @@ const SignLogin = (props) => {
             required
           ></input>
         </label>
-        <input type="submit" id="submit" value="ثبت"></input>
+        <input type="submit" className="loginButtons" value="ورود"></input>
       </form>
+      {/* //////////////////////////////////////////////////// finish login*/}
+
+      {/* //////////////////////////////////////////////////// student*/}
+      <form
+        id="signupContinueStudent"
+        onSubmit={handleStudentSignup}
+        style={{ display: "none" }}
+      >
+        <label>
+          نام و نام خانوادگی
+          <br />
+          <input
+            type="text"
+            value={name}
+            onChange={handleName}
+            size="40"
+            required
+          ></input>
+        </label>
+        <label>
+          کد ملی
+          <br />
+          <input
+            type="number"
+            value={code}
+            onChange={handleCode}
+            size="40"
+            required
+            // onWheelCapture={(e) => {
+            //   e.target.blur();
+            // }}
+          ></input>
+        </label>
+        <label>
+          جنسیت
+          <br />
+          <select value={gender} onChange={handleGender} required>
+            <option value="0">مرد</option>
+            <option value="1">زن</option>
+          </select>
+        </label>
+        <label>
+          شماره دانشجویی
+          <br />
+          <input
+            type="number"
+            value={studentId}
+            onChange={handleStudentId}
+            size="40"
+            required
+            // onWheelCapture={(e) => {
+            //   e.target.blur();
+            // }}
+          ></input>
+        </label>
+        <input type="submit" className="loginButtons" value="ثبت"></input>
+      </form>
+      {/* //////////////////////////////////////////////////// finish student*/}
+
+      {/* //////////////////////////////////////////////////// teacher*/}
+      <form
+        id="signupContinueTeacher"
+        onSubmit={handleTeacherSignup}
+        style={{ display: "none" }}
+      >
+        <label>
+          نام و نام خانوادگی
+          <br />
+          <input
+            type="text"
+            value={name}
+            onChange={handleName}
+            size="40"
+            required
+          ></input>
+        </label>
+        <label>
+          کد ملی
+          <br />
+          <input
+            type="number"
+            value={code}
+            onChange={handleCode}
+            size="40"
+            required
+            onWheelCapture={(e) => {
+              e.target.blur();
+            }}
+          ></input>
+        </label>
+        <label>
+          جنسیت
+          <br />
+          <select value={gender} onChange={handleGender} required>
+            <option value="0">مرد</option>
+            <option value="1">زن</option>
+          </select>
+        </label>
+        <label>
+          شماره پرسنلی
+          <br />
+          <input
+            type="number"
+            value={teacherId}
+            onChange={handleTeacherId}
+            size="40"
+            required
+            onWheelCapture={(e) => {
+              e.target.blur();
+            }}
+          ></input>
+        </label>
+        <input type="submit" className="loginButtons" value="ثبت"></input>
+      </form>
+      {/* //////////////////////////////////////////////////// finish teacher*/}
+
+      {/* //////////////////////////////////////////////////// company*/}
+      <form
+        id="signupContinueCompany"
+        onSubmit={handleCompanySignup}
+        style={{ display: "none" }}
+      >
+        <label>
+          نام شرکت
+          <br />
+          <input
+            type="text"
+            value={companyName}
+            onChange={handleCompanyName}
+            size="40"
+            required
+          ></input>
+        </label>
+        <label>
+          کد اقتصادی
+          <br />
+          <input
+            type="number"
+            value={companyCode}
+            onChange={handleCompanyCode}
+            size="40"
+            required
+            onWheelCapture={(e) => {
+              e.target.blur();
+            }}
+          ></input>
+        </label>
+
+        <input type="submit" className="loginButtons" value="ثبت"></input>
+      </form>
+      {/* //////////////////////////////////////////////////// finish company*/}
     </div>
   );
 };
